9f0541f912d06f63f1147b962bce6cf1803522fd
Didier Fabert <dfabert@b2pweb.com>
2014-07-16 06:33:14 +0200

    Modify header file: change GPL version (v3+), change FSF address

--- src/durl.h
+++ src/durl.h
@@ -1,44 +1,44 @@
 /******************************************************************************
-*      ____              ____    _       _   _                               *
-*     /# /_\_           |  _ \  (_)   __| | (_)   ___   _ __                 *
-*    |  |/o\o\          | | | | | |  / _` | | |  / _ \ | '__|                *
-*    |  \\_/_/          | |_| | | | | (_| | | | |  __/ | |                   *
-*   / |_   |            |____/  |_|  \__,_| |_|  \___| |_|                   *
-*  |  ||\_ ~|                                                                *
-*  |  ||| \/                                                                 *
-*  |  |||       Project : DLibs : usefull tools for C++ programming          *
-*  \//  |                                                                    *
-*   ||  |       Developper : Didier FABERT <didier.fabert@gmail.com>         *
-*   ||_  \      Date : 2009, April                                           *
-*   \_|  o|                                             ,__,                 *
-*    \___/      Copyright (C) 2009 by didier fabert     (oo)____             *
-*     ||||__                                            (__)    )\           *
-*     (___)_)   File : durl.h                              ||--|| *          *
-*                                                                            *
-*   This program is free software; you can redistribute it and/or modify     *
-*   it under the terms of the GNU General Public License as published by     *
-*   the Free Software Foundation; either version 2 of the License, or        *
-*   (at your option) any later version.                                      *
-*                                                                            *
-*   This program is distributed in the hope that it will be useful,          *
-*   but WITHOUT ANY WARRANTY; without even the implied warranty of           *
-*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            *
-*   GNU General Public License for more details.                             *
-*                                                                            *
-*   You should have received a copy of the GNU General Public License        *
-*   along with this program; if not, write to the                            *
-*   Free Software Foundation, Inc.,                                          *
-*   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.                *
-******************************************************************************/
+ *      ____              ____    _       _   _                               *
+ *     /# /_\_           |  _ \  (_)   __| | (_)   ___   _ __                 *
+ *    |  |/o\o\          | | | | | |  / _` | | |  / _ \ | '__|                *
+ *    |  \\_/_/          | |_| | | | | (_| | | | |  __/ | |                   *
+ *   / |_   |            |____/  |_|  \__,_| |_|  \___| |_|                   *
+ *  |  ||\_ ~|                                                                *
+ *  |  ||| \/                                                                 *
+ *  |  |||       Project : DLibs : usefull tools for C++ programming          *
+ *  \//  |                                                                    *
+ *   ||  |       Developper : Didier FABERT <didier.fabert@gmail.com>         *
+ *   ||_  \      Date : 2009, April                                           *
+ *   \_|  o|                                             ,__,                 *
+ *    \___/      Copyright (C) 2009 by didier fabert     (oo)____             *
+ *     ||||__                                            (__)    )\           *
+ *     (___)_)   File : durl.h                              ||--|| *          *
+ *                                                                            *
+ *   This program is free software; you can redistribute it and/or modify     *
+ *   it under the terms of the GNU General Public License as published by     *
+ *   the Free Software Foundation; either version 3 of the License, or        *
+ *   (at your option) any later version.                                      *
+ *                                                                            *
+ *   This program is distributed in the hope that it will be useful,          *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of           *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            *
+ *   GNU General Public License for more details.                             *
+ *                                                                            *
+ *   You should have received a copy of the GNU General Public License        *
+ *   along with this program; if not, write to the                            *
+ *   Free Software Foundation, Inc.,                                          *
+ *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.             *
+ ******************************************************************************/
 
 #ifndef DURL_H
-#define DURL_H
+ #define DURL_H
 
-#include "dstring.h"
+ #include "dstring.h"
 
 using namespace std;
 
-
+ 
 /**
 * Class to manage network address
 * @include testdurl.cpp
@@ -60,7 +60,7 @@ public:
 
 	/**
 	* Default constructor
-	* @param url the url
+ 	* @param url the url
 	*/
 	DURL(DString url);
 
--- tests/testdstring.h
+++ tests/testdstring.h
@@ -1,3 +1,42 @@
+/******************************************************************************
+ *      ____              ____    _       _   _                               *
+ *     /# /_\_           |  _ \  (_)   __| | (_)   ___   _ __                 *
+ *    |  |/o\o\          | | | | | |  / _` | | |  / _ \ | '__|                *
+ *    |  \\_/_/          | |_| | | | | (_| | | | |  __/ | |                   *
+ *   / |_   |            |____/  |_|  \__,_| |_|  \___| |_|                   *
+ *  |  ||\_ ~|                                                                *
+ *  |  ||| \/                                                                 *
+ *  |  |||       Project : DLibs : usefull tools for C++ programming          *
+ *  \//  |                                                                    *
+ *   ||  |       Developper : Didier FABERT <didier.fabert@gmail.com>         *
+ *   ||_  \      Date : 2009, April                                           *
+ *   \_|  o|                                             ,__,                 *
+ *    \___/      Copyright (C) 2009 by didier fabert     (oo)____             *
+ *     ||||__                                            (__)    )\           *
+ *     (___)_)   File : testdstring.h                       ||--|| *          *
+ *                                                                            *
+ *   This program is free software; you can redistribute it and/or modify     *
+ *   it under the terms of the GNU General Public License as published by     *
+ *   the Free Software Foundation; either version 2 of the License, or        *
+ *   (at your option) any later version.                                      *
+ *                                                                            *
+ *   This program is distributed in the hope that it will be useful,          *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of           *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            *
+ *   GNU General Public License for more details.                             *
+ *                                                                            *
+ *   You should have received a copy of the GNU General Public License        *
+ *   along with this program; if not, write to the                            *
+ *   Free Software Foundation, Inc.,                                          *
+ *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.             *
+ *                                                                            *
+ *   Unit Test for DString                                                    *
+ *                                                                            *
+ ******************************************************************************/
+
+#ifndef _TESTDSTRING_H
+#define _TESTDSTRING_H
+
 #include <stdio.h>
 #include <cpptest.h>
 
@@ -44,3 +83,5 @@ private:
 	void justify_test();
 	void contains_test();
 };
+
+#endif // _TESTDSTRING_H
