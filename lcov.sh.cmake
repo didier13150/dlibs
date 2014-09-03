2a319a24eb8c8b8064fe11ba60fef4a05bab6244
Didier Fabert <dfabert@b2pweb.com>
2014-09-03 15:32:23 +0200

    Improve lcov target: change flag: O0 and add -Wextra

--- lcov.sh.cmake
+++ lcov.sh.cmake
@@ -1,32 +1,29 @@
 #!/bin/bash
 
-OUTPUT_DIR="@CMAKE_SOURCE_DIR@"
-
 # Clean up
 make clean
 rm -rf CMakeFiles src Testing tests
 
 find . -name '*.info' -delete
-find . -name '*.gcda' -delete
 rm -rf ../lcov/??*
 
 # Reset lcov
-lcov --zerocounters --directory ${OUTPUT_DIR}
+lcov --zerocounters --directory @CMAKE_SOURCE_DIR@
 
 # Build project
 cmake -DWITH_LCOV=ON -DCMAKE_BUILD_TYPE=Debug ..
-make
+make -j$(nproc)
 
 # Init lcov
-lcov --no-external --capture --initial --directory ${OUTPUT_DIR} --output-file base.info
+lcov --no-external --follow --capture --initial --directory @CMAKE_SOURCE_DIR@ --output-file base.info
 
 # Run test
 make -k test
 
 # lcov
-lcov --no-external --capture --directory ${OUTPUT_DIR} --output-file test.info
+lcov --no-external --follow --capture --directory @CMAKE_SOURCE_DIR@ --output-file test.info
 lcov --add-tracefile base.info --add-tracefile test.info --output-file total.info
-lcov --remove total.info '/usr/include/*' '/usr/lib/*' 'src/sqlite3.*' --output-file filtered.info
-#cp total.info filtered.info
 
-genhtml --ignore-errors source filtered.info --legend --title "DLibs libraries" --output-directory=${OUTPUT_DIR}/lcov
+lcov --remove total.info '/usr/include/*' '/usr/lib*/*' 'src/sqlite3.*' 'src/dlibspath.cpp' --output-file filtered.info
+
+genhtml --ignore-errors source filtered.info --legend --title "DLibs libraries @DLIBS_VERSION@" --output-directory=@CMAKE_SOURCE_DIR@/lcov
