89c9dc3a732820c6ce6fcf18e30b5ffa35bdff37
Didier Fabert <dfabert@b2pweb.com>
2014-09-04 12:27:11 +0200

    Remove --no-external opt

--- lcov.sh.cmake
+++ lcov.sh.cmake
@@ -15,13 +15,13 @@ cmake -DWITH_LCOV=ON -DCMAKE_BUILD_TYPE=Debug ..
 make -j$(nproc)
 
 # Init lcov
-lcov --no-external --follow --capture --initial --directory @CMAKE_SOURCE_DIR@ --output-file base.info
+lcov --follow --capture --initial --directory @CMAKE_SOURCE_DIR@ --output-file base.info
 
 # Run test
 make -k test
 
 # lcov
-lcov --no-external --follow --capture --directory @CMAKE_SOURCE_DIR@ --output-file test.info
+lcov --follow --capture --directory @CMAKE_SOURCE_DIR@ --output-file test.info
 lcov --add-tracefile base.info --add-tracefile test.info --output-file total.info
 
 lcov --remove total.info '/usr/include/*' '/usr/lib*/*' 'src/sqlite3.*' 'src/dlibspath.cpp' --output-file filtered.info
