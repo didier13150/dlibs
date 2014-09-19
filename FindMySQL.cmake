044dcb9a91ed32bc4c2e835586accf6e61eab569
Didier Fabert <didier.fabert@gmail.com>
2014-09-19 06:41:57 +0200

    BUGFIX: find library by name one by one

--- FindMySQL.cmake
+++ FindMySQL.cmake
@@ -7,22 +7,34 @@
 #  MYSQL_FOUND       - True if MySQL found.
 ################################################################################
 
-SET( MYSQL_LIB_NAMES mysqlclient mysqlclient_r )
-find_library( MYSQL_LIBRARY
-              NAMES ${MYSQL_LIB_NAMES}
-              PATH /usr/lib /usr/local/lib /usr/lib64 /usr/local/lib64 /opt/mysql/lib /opt/mysql/lib64
+SET( MYSQL_LIB_PATHS
+     /usr/lib
+     /usr/lib64
+     /usr/lib/mysql
+     /usr/lib64/mysql
+     /usr/local/lib
+     /usr/local/lib64
+     /usr/local/lib/mysql
+     /usr/local/lib64/mysql
+     /opt/mysql/lib
+     /opt/mysql/lib64
 )
-             
-find_path( MYSQL_INCLUDE_DIR
-           mysql.h
-           /usr/include
-           /usr/include/mysql
-           /usr/local/include
-           /usr/local/include/mysql
-           /opt/mysql/include
-           
+
+SET( MYSQL_INCLUDE_PATHS
+     /usr/include
+     /usr/include/mysql
+     /usr/local/include
+     /usr/local/include/mysql
+     /opt/mysql/include
 )
-          
+
+find_library( MYSQL_LIBRARY mysqlclient ${MYSQL_LIB_PATHS} )
+if( NOT MYSQL_LIBRARY )
+    find_library( MYSQL_LIBRARY mysqlclient_r ${MYSQL_LIB_PATHS} )
+endif( NOT MYSQL_LIBRARY )
+
+find_path( MYSQL_INCLUDE_DIR mysql.h ${MYSQL_INCLUDE_PATHS} )
+
 include( ${CMAKE_ROOT}/Modules/FindPackageHandleStandardArgs.cmake )
 FIND_PACKAGE_HANDLE_STANDARD_ARGS( MYSQL REQUIRED_VARS MYSQL_LIBRARY MYSQL_INCLUDE_DIR )
 
